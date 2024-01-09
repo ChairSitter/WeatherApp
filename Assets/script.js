786b06dc7282dc429c533e9d58785ee9b0b06c79
Bryan Schneller <bryan.schneller@gmail.com>
2024-01-09 10:23:59 -0500

    Add reset stylesheet

--- Assets/script.js
+++ Assets/script.js
@@ -1,25 +1,27 @@
 //API key = bd3136b1de02f5dec5a45d5eb3dea4e9
-//API url: 
+//Displayed per city: Date, Icon representing weather, temp, humidity, wind speed
 
-let city = "Raleigh, NC";
+
+
+const getWeatherData = async(city, state, country) => {
+    const getLatLonResponse= await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city},${state},${country}&appid=bd3136b1de02f5dec5a45d5eb3dea4e9`);
+    const latLonData = await getLatLonResponse.json();
+    const weatherResponse = await fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${latLonData[0].lat}&lon=${latLonData[0].lon}&units=imperial&appid=bd3136b1de02f5dec5a45d5eb3dea4e9`);
+    const weatherData = await weatherResponse.json();
+    let weatherArray = [];
+    for(let i = 0; i < weatherData.list.length; i += 8){
+           console.log("Date: " + weatherData.list[i].dt_txt); 
+           console.log("Weather: " + weatherData.list[i].weather[0].main); 
+           console.log("Temp: " + weatherData.list[i].main.temp); 
+           console.log("Humidity: " + weatherData.list[i].main.humidity); 
+           console.log("Wind speed: " + weatherData.list[i].wind.speed); 
+    }
+}
+
+let city = "Raleigh";
 let state = "NC"; 
-let country = "US"
+let country = "US";
 
-const getLatLon = async(url) => {
-    const response = await fetch(url);
-    const data = await response.json();
-    let latLon = [data[0].lat, data[0].lon];
-    return latLon;
-}
+getWeatherData(city, state, country);
 
-const latLon = getLatLon(`http://api.openweathermap.org/geo/1.0/direct?q=${city},${state},${country}&appid=bd3136b1de02f5dec5a45d5eb3dea4e9`);
-
-const getWeatherData = async(url) => {
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log(data);
-}
-
-
-getWeatherData(`http://api.openweathermap.org/data/2.5/forecast?lat=${latLon[0]}&lon=${latLon[1]}&appid=bd3136b1de02f5dec5a45d5eb3dea4e9`);
 
